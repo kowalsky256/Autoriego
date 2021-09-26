a23616398361316e6d8b5a458473373c1958046a
kowalsky <ya_pasa@hotmail.com>
2021-09-26 22:54:46 +0200

    Done work flow

--- README.md
+++ README.md
@@ -8,13 +8,17 @@
 
 ## Lindars
 Sensor1 >= 75 --> Regar
+
 Sensor2 >= 20 --> Regar
+
 Sensor3 >= 34 --> Regar
 
 
 
 Sensor1 <= 34 --> Parar Regar
+
 Sensor2 <= ¿¿27?? --> Parar Regar
+
 Sensor3 <= -60 --> Parar Regar
 
 
--- autoriego.ino
+++ autoriego.ino
@@ -3,6 +3,7 @@ int sensor1Pin = A0;    // select the input pin for the humidity sensor 1
 int sensor2Pin = A1;    // select the input pin for the humidity sensor 2
 int sensor3Pin = A2;    // select the input pin for the humidity sensor 3
 
+
 int relay1Pin = 2;      // select the pin for the relay 1  ---> Activar Relay
 int relay2Pin = 3;      // select the pin for the relay 2
 int relay3Pin = 4;      // select the pin for the relay 3
@@ -11,14 +12,18 @@ int pump1Pin = 12;      // sepect the pin for the pump
 int sensorValue;  // variable to store the value coming from the sensor
 int secco; // variable to store the value coming from the mapping of sensorValue
 int input;
-bool regando;
+//bool regando;
 const int buttonPin=6;
 int buttonState=0;
 bool botton_ON=false;
 
+int LIMITE_SECO[3] = {34, 27,-60};
+int LIMITE_HUMEDO[3] = {75, 20, 34};
+int NUM_SENSOR[3] = {1,2,3};
 
-int LIMITE_HUMEDO=55;
-int LIMITE_SECO=75;
+bool isWatering[3] = {false, false, false};
+
+bool IS_TEST=true;
 
 
 void setup() {
@@ -31,112 +36,92 @@ void setup() {
   
   pinMode(pump1Pin, OUTPUT);
 
-  //InitialState
+  //Disable relays (HIGH)
   digitalWrite(relay1Pin, HIGH);
   digitalWrite(relay2Pin, HIGH);
   digitalWrite(relay3Pin, HIGH);
   
   digitalWrite(pump1Pin, LOW);
-  regando=false;
   
 }
 
 void loop() {
-  // FOR TEST
-  /*regando=false;
-  for (input=100; input >= 0; input=input-1){
-    doTest(input, relay1Pin, pump1Pin);
-    delay(1000);   
-  }
+  
+  //CHECK MESURES: FOR TEST
+  //doCheckMesure();
 
-  regando=true;
-  input=0;
-  for (input=0; input <= 100; input=input+1){
-    doTest(input, relay1Pin, pump1Pin);
-    delay(1000);   
-  }*/
-  digitalWrite(relay1Pin, HIGH);
-  digitalWrite(relay2Pin, HIGH);
-  digitalWrite(relay3Pin, HIGH);
+  //MAIN CONTROL
+  doTask();
+  
+
+  delay(10000);        // delay in between reads for stability
+}
+
+
+
+void doTask(){
+  task(sensor1Pin, relay1Pin, pump1Pin, NUM_SENSOR[0]);
+  //task(sensor2Pin, relay2Pin, pump1Pin, NUM_SENSOR[1]);
+  task(sensor3Pin, relay3Pin, pump1Pin, NUM_SENSOR[2]);
+  if(IS_TEST){
+     Serial.println();
+     Serial.println("*******************");
+  }
+  
+}
+
+
+void task (int sensor, int relay, int pumpIn, int numSensor) {
+  
+  int limiteSeco=LIMITE_SECO[numSensor-1];
+  int limiteHumedo=LIMITE_HUMEDO[numSensor-1];
+  bool regando = isWatering[numSensor-1];
+  
+  sensorValue = analogRead(sensor);
+  secco = map( sensorValue, 200,500, 0,100);
+  
+
+  if (secco >= limiteSeco && !regando){ 
+     digitalWrite(relay, LOW);    //to activate the solenoid valve relay
+     digitalWrite(pumpIn, LOW);     //to activate the pump relay
+     isWatering[numSensor-1]=true;
+     
+   }
+   else if (secco <= limiteHumedo && regando){
+     digitalWrite(relay, HIGH);
+     digitalWrite(pumpIn, HIGH);
+     isWatering[numSensor-1]=false;
+     
+   }
+
+   if(IS_TEST){
+     printResult(numSensor, regando, secco);
+   }
+}
+
+void printResult(int numSensor, bool regando, int secco){
+  String estaRegando = "NO regando";
+  if(regando){
+      estaRegando = "Regando";
+  }
+  Serial.print("Sensor ");
+  Serial.print(numSensor);
+  Serial.print(": ");
+  Serial.print(secco);
+  Serial.print("--> ");
+  Serial.println(estaRegando);
+}
+
+void doCheckMesure(){
+  
   checkMesure(sensor1Pin, relay1Pin, 1);
   checkMesure(sensor2Pin, relay2Pin, 2);
   checkMesure(sensor3Pin, relay3Pin, 3);
+  doCheckMesure();
   Serial.println();
   Serial.println("*******************");
-  delay(10000);        // delay in between reads for stability
-   /* if (!botton_ON){
   
-     //doTestCapacitador(A2, relay2Pin, pump1Pin, 2);
-     //doTestCapacitador(A1, relay1Pin, pump1Pin, 1);
-     //doTask(sensor1Pin, relay1Pin, pump1Pin);
-     //doTask(sensor2Pin, relay2Pin, pump1Pin);
-     //doTask(sensor3Pin, relay3Pin, pump1Pin);
-     
-  }
-  else{
-    digitalWrite(relay1Pin, HIGH);
-    digitalWrite(relay2Pin, HIGH);
-    digitalWrite(relay3Pin, HIGH);
-  }*/
 }
-
-
-
-void doTestCapacitador (int sensor, int relay, int pumpIn, int number) {
-  sensorValue = analogRead(sensor);
-  //Serial.println(sensorValue);
-
-  secco = map( sensorValue, 15,650, 0,100);
-  Serial.println(String(number) + ": " + String(secco) );
-
-  /*if(regando){
-    Serial.println("regando");
-  }else{
-    Serial.println("NO regando");
-  }
-  Serial.println(secco);
-
-  if (secco >= LIMITE_SECO && !regando){ 
-     digitalWrite(relay, HIGH);    //to activate the solenoid valve relay
-     digitalWrite(pumpIn, HIGH);     //to activate the pump relay
-     regando=true;
-     
-   }
-   else if (secco <= LIMITE_HUMEDO && regando){
-     digitalWrite(relay, LOW);
-     digitalWrite(pumpIn, LOW);
-     regando=false;
-     
-   }*/
-}
-
-
-void doTask (int sensor, int relay, int pumpIn) {
-  sensorValue = analogRead(sensor);
-  secco = map( sensorValue, 200,500, 0,100);
-
-  if(regando){
-    Serial.println("regando");
-  }else{
-    Serial.println("NO regando");
-  }
-  Serial.println(secco);
-
-  if (secco >= LIMITE_SECO && !regando){ 
-     digitalWrite(relay, HIGH);    //to activate the solenoid valve relay
-     digitalWrite(pumpIn, HIGH);     //to activate the pump relay
-     regando=true;
-     
-   }
-   else if (secco <= LIMITE_HUMEDO && regando){
-     digitalWrite(relay, LOW);
-     digitalWrite(pumpIn, LOW);
-     regando=false;
-     
-   }
-}
-
-
 
 void checkMesure(int sensor, int relay, int numSensor){
   sensorValue = analogRead(sensor);
@@ -146,30 +131,4 @@ void checkMesure(int sensor, int relay, int numSensor){
   Serial.print(numSensor);
   Serial.print(": ");
   Serial.println(secco);
-}
-
-
-
-
-void doTest (int sensor, int relay, int pumpIn) {
-  secco=sensor;
-  if(regando){
-    Serial.println("regando");
-  }else{
-    Serial.println("NO regando");
-  }
-  Serial.println(secco);
-
-  if (secco >= LIMITE_SECO && !regando){ 
-     digitalWrite(relay, HIGH);    //to activate the solenoid valve relay
-     digitalWrite(pumpIn, HIGH);     //to activate the pump relay
-     regando=true;
-     
-   }
-   else if (secco <= LIMITE_HUMEDO && regando){
-     digitalWrite(relay, LOW);
-     digitalWrite(pumpIn, LOW);
-     regando=false;
-     
-   }
 }
